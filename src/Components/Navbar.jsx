6ac0d4150ecd6c4ea2f142e90c0d606810634b5d
anojfernando <anojfern@gmail.com>
2022-10-17 19:17:47 +0530

    moved bootstrap from nabar.jsx to index.js

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
-import * as MdIcons from "react-icons/md";
+//import * as MdIcons from "react-icons/md";
 
 import { NavbarData } from './NavbarData'
 import './Navbar.css'
