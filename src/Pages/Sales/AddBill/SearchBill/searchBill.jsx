566688be6c9d09076807ecc86e485440d683cb34
Binura Thiranjaya <binura.thiranjaya@nichelabs.lk>
2022-11-22 00:25:44 +0530

    search Bill with json array

--- src/Pages/Sales/AddBill/SearchBill/searchBill.jsx
+++ src/Pages/Sales/AddBill/SearchBill/searchBill.jsx
@@ -3,27 +3,54 @@ import React from 'react'
 export default function searchBill() {
 
   function SearchUsingInput() {
-    var json = {
-      "BillNo": "1",
-      "Quantity": "1",
-      "Price": "1",
-      "Total": "1",
-      "Paid": "1",
-    };
+    var json = 
+      [
+        {
+          "BillNo": "1",
+          "Quantity": "1",
+          "Price": "1",
+          "Total": "1",
+          "Paid": "1"
+        },
+        {
+          "BillNo": "200003",
+          "Quantity": "1",
+          "Price": "1",
+          "Total": "1",
+          "Paid": "1"
+        },
+        {
+          "BillNo": "30001",
+          "Quantity": "1",
+          "Price": "1",
+          "Total": "1",
+          "Paid": "1"
+        },
+        {
+          "BillNo": "40001",
+          "Quantity": "1",
+          "Price": "1",
+          "Total": "1",
+          "Paid": "1"
+        }
+
+      ];
     var input = document.getElementById("search").value;
-    //check whether the input is name as Bill No
-    if (input == json.BillNo) {
-      //if the input is Bill No, then display the details in a table
-      document.getElementById("table").innerHTML = "<tr><th>Bill No</th><th>Quantity</th><th>Price</th><th>Total</th><th>Paid</th></tr><tr><td>" + json.BillNo + "</td><td>" + json.Quantity + "</td><td>" + json.Price + "</td><td>" + json.Total + "</td><td>" + json.Paid + "</td></tr>";
-    }else{
-      //if the input is not Bill No, then display the message
-      document.getElementById("table").innerHTML = "No Bill Found";
-    }
+      for(var i=0;i<json.length;i++){
+       if (input === json[i].BillNo) {
+         document.getElementById("table").innerHTML = "<tr><th>Bill No</th><th>Quantity</th><th>Price</th><th>Total</th><th>Paid</th></tr><tr><td>" + json[i].BillNo + "</td><td>" + json[i].Quantity + "</td><td>" + json[i].Price + "</td><td>" + json[i].Total + "</td><td>" + json[i].Paid + "</td></tr>";
+         break;
+
+        }else{
+          document.getElementById("table").innerHTML = "No Bill Found";
+        }
+      }
+    
   }
   return (
     <div className="container">
-      <div class="input-group mb-3 mt-2">
-          <input type="text" class="form-control" id="search" placeholder="Enter Bill Number" aria-label="Enter Bill Number" aria-describedby="basic-addon2" onChange={SearchUsingInput}/>
+      <div className="input-group mb-3 mt-2">
+          <input type="text" className="form-control" id="search" placeholder="Enter Bill Number" aria-label="Enter Bill Number" aria-describedby="basic-addon2" onChange={SearchUsingInput}/>
         </div>
         <div>
         </div>
