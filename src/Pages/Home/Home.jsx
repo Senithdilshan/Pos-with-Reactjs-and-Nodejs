9ab5c6634a2ce11f11c8e2f57a50ef83b12bec3d
A.M.S Dilshan Amarasinghe <81614964+Senithdilshan@users.noreply.github.com>
2022-11-23 20:43:56 +0530

    home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,25 @@
+import { Blockquote, Center } from '@mantine/core'
 import React from 'react'
 import Navbar from '../../Components/Navbar'
-
+import './Home.css'
+import logo from "./logo.png"
 export default function Home() {
   return (
     <div>
-        <Navbar/>
-        <h1>Home</h1>
+      <Navbar />
+      <div className="Auth-form-container">
+        <div className="Auth-form" >
+          <div className="Auth-form-content">
+            <h1 className="Auth-form-title1">Welcome To Dealz Super</h1>
+            <div className="background">
+              <img className='img' src={logo} alt="" />
+              <p className='head'>The Future Of Grocery Stores Are Bright</p>
+              <p>Developed By</p>
+              <p className='dev'>Team Semicolon</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
